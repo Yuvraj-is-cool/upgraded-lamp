849c4d0c98df6b7e547362f9b7b1d100ef250f50
Yuvraj-is-cool <99299241+Yuvraj-is-cool@users.noreply.github.com>
2023-03-02 16:45:41 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,23 @@
 function preload(){
-    clown_nose=loadImage('https://i.postimg.cc/FRXqZLfy/clown-nose.png');
 }
-noseX=0;
-noseY=0;
 function setup(){
-    canvas=createCanvas(500,500);
-    canvas.center();
-    canvas.position(600,150);
+    canvas=createCanvas(730,510);
+    canvas.position(100,20);
     video=createCapture(VIDEO);
-    video.size(500,500);
     video.hide();
-
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
-}
-function gotPoses(results){
-    if(results.length>0){
-        console.log(results);
-        noseX= results[0].pose.nose.x-15;
-        noseY= results[0].pose.nose.y-15;
-        console.log("nose x= "+ results[0].pose.nose.x);
-        console.log("nose y= "+ results[0].pose.nose.y);
-    }
-}
-function modelLoaded(){
-    console.log('PoseNet Is Initialized');
+  
+    
 }
 function draw(){
-    image(video, 0,0,500,500);
-    fill(255,0,0);
-    stroke(255,0,0);
-    
-    image(clown_nose,noseX,noseY,50,50);
+    circle(30, 30, 40);
+    fill(255,0,0)
+    circle(30, 510, 40);
+    fill(255,0,0)
+    circle(730, 30, 40);
+    fill(255,0,0)
+    circle(730, 510, 40);
+    fill(255,0,0)
+    rect(30, 20, 680, 500);
+    image(video,30,20,680,500);
 }
-function take_snapshot(){
-    save('something_special_but_better.png');
-}
+ 
